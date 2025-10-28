77b5f054422e2f7eb8ee7cbf1799e3c40cfd7096
Asos75 <andraz.sosteric@gmail.com>
2025-10-28 17:51:29 +0100

    Added GameOverScreen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
         Debug.Log("Play Game button clicked");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
     }
 
     public void OpenSettings()
